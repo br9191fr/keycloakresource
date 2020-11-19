cd792b23101762a0f6a13ef64c91833ebff07136
Bruno Ricci <brricci@orange.fr>
2020-11-19 12:20:19 +0100

    Add displaySubscriberId

--- app.js
+++ app.js
@@ -61,8 +61,9 @@ let displayToken = function(req, res) {
       }
       else{
         // verifiedJwt will contain the payload of the jwt
-        console.log('Token is OK');
-
+        console.log('In displayToken : Token is OK');
+        console.log('In displayToken : Last Token =>\n'+JSON.stringify(jwt.decode(lastToken)));
+        console.log('In displayToken : Verified Token =>\n'+JSON.stringify(verifiedJwt));
         const segments = lastToken.split('.');
 
         const headerSeg = segments[0];
@@ -107,6 +108,22 @@ let displayFruit =  function(req, res) {
   let result = ["Apple","Pear","Grape","Orange","Other"];
   res.json(result);
 }
+let displaySubscriberId = function(req,res) {
+  let auth = req.headers['authorization'];
+  let token = auth.slice('bearer '.length);
+  let decode = jwt.decode(token);
+  let subscriber = decode.sub;
+  let expires_in = decode.exp;
+
+  let exp = new Date(expires_in * 1000).toISOString().substr(11, 8);
+  let result = {"subscriber" : subscriber};
+  console.log('In Display Subscriber. Token =>\n'+token);
+  console.log('In Display Subscriber. Subscriber =>\n'+subscriber);
+  console.log('In Display Subscriber. Result =>\n'+result.subscriber);
+  console.log('In Display Subscriber. Expires at =>\n'+exp);
+  res.json(result);
+}
+
 let displaySubscriber = function(req,res) {
   let auth = req.headers['authorization'];
   let token = auth.slice('bearer '.length);
